02e158c781b3ab36bcbabf0409742a1052e60fed
Jeff59550 <38660219+Jeff59550@users.noreply.github.com>
2019-03-22 15:02:25 +0100

    Update example.sql
    Add filename / filepath & smpt_server parameters in example.

--- example.sql
+++ example.sql
@@ -1,16 +1,20 @@
-select py_pgmail(
-	'sentFromEmail@whatever.com',
-	array['dest@email.com','dest2@email.com'],
-	array['cc@email.com','cc2@email.com','cc3@email.com'],
-	array['bcc@email.com','bcc2@email.com','bcc3@email.com'],
-	'My subject',
-	'<USERNAME>',
-	'<PASSWORD>',
-	'Text message! This is an email sent from a database!',
+SELECT py_pgmail(
+	'sentFromEmail@whatever.com',					-- from
+	array['dest@email.com','dest2@email.com'],			-- to
+	array['cc@email.com','cc2@email.com','cc3@email.com'],		-- cc
+	array['bcc@email.com','bcc2@email.com','bcc3@email.com'],	-- bcc
+	'My subject',							-- subject
+	'<USERNAME>',							-- login
+	'<PASSWORD>',							-- password
+	'Text message! This is an email sent from a database!',		-- msg_text
 	'<!DOCTYPE html>
 		<html>
-		<body>
-		<p>html message! This is an email sent from a database!</p>
-		</body>
-		</html>
-');
+			<body>
+				<p>html message! This is an email sent from a database!</p>
+			</body>
+		</html>',						-- msg_html
+	'attachment.csv',   						-- filename
+	'/foo/bar/',   							-- filepath
+	'smtpServer.whatever.com'					-- smtp_server
+);
+	
